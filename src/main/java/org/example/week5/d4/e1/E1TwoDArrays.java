a048bb6bbb7cbb5d492179ff5c635629918f32d9
Ranjana Sharma <Ranjana2171@gmail.com>
2026-03-15 12:56:39 -0400

    change in week4.d5 instead of week5.d5

--- src/main/java/org/example/week5/d4/e1/E1TwoDArrays.java
+++ src/main/java/org/example/week5/d4/e1/E1TwoDArrays.java
@@ -12,7 +12,7 @@ public class E1TwoDArrays {
             for (int j = 0; j < 3; j++) {
                 System.out.print(matrix[i][j] + " ");
             }
-            System.out.println(); // move to next row
+            System.out.println();
         }
     }
 }
